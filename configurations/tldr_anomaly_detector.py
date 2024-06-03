95596e2611a3405ed0a6fc7abed6e3b12b9b2307
JoottunAtish <ajoottun24@gmail.com>
2024-06-03 14:57:00 +0400

    adding the tldr anomaly detector

---
+++ configurations/tldr_anomaly_detector.py
@@ -0,0 +1,25 @@
+import concurrent.futures
+from tldr_fail_test import tldr_dectector
+
+
+
+def process(ip_addresses, num_of_threads, chunk_size, Processed_ip_addresses = []):
+    number_of_ip_addresses = len(ip_addresses)
+
+    for i in range(0, number_of_ip_addresses, chunk_size):
+        with concurrent.futures.ThreadPoolExecutor(num_of_threads) as executor:
+            futures = {executor.submit(tldr_dectector, ip): ip for ip in ip_addresses[i:i+chunk_size]}
+            
+            for future in concurrent.futures.as_completed(futures):
+                BinaryEncoding = futures[future]
+                try:
+                    if future.result():
+                        Processed_ip_addresses.append(BinaryEncoding)
+                    else:
+                        Processed_ip_addresses.append(BinaryEncoding)
+                except Exception as e:
+                    print(e)
+                    Processed_ip_addresses.append(BinaryEncoding)
+
+    return Processed_ip_addresses
+
--- configurations/tldr_fail_test.py
+++ configurations/tldr_fail_test.py
@@ -19,9 +19,8 @@
 #
 # You can optionally pass the destination IP with --addr
 #
-# Author: davidben [at] chromium [dot] org
+# Author: davidben [at] chromium [dot] org (David Benjamin)
 
-import argparse
 import socket
 import time
 
@@ -134,7 +133,6 @@ def make_client_hello(name, kyber):
 
 
 def tldr_dectector(host, addr = None, port = 443):
-
     BinaryEncoding = ""
 
     if addr is None:
@@ -144,70 +142,75 @@ def tldr_dectector(host, addr = None, port = 443):
 
     client_hello = make_client_hello(host, kyber=True)
 
-    print(f"About to send a large TLS ClientHello ({len(client_hello)} bytes) to {addr[0]}:{addr[1]}.")
-    print()
-    print("The server should respond with a TLS ServerHello, which will be some")
-    print("byte string beginning with b'\\x16\\x03\\x03'. If it closes the")
-    print("connection or sends something else, the server is misbehaving.")
-    print()
+    # print(f"About to send a large TLS ClientHello ({len(client_hello)} bytes) to {addr[0]}:{addr[1]}.")
+    # print()
+    # print("The server should respond with a TLS ServerHello, which will be some")
+    # print("byte string beginning with b'\\x16\\x03\\x03'. If it closes the")
+    # print("connection or sends something else, the server is misbehaving.")
+    # print()
 
     print("Sending the large ClientHello in a single write:")
-    sock = socket.create_connection(addr)
     try:
+        sock = socket.create_connection(addr)
         sock.send(client_hello)
-        print(sock.recv(256) , "\n L1-Flag:1")
+        print(sock.recv(256))
         BinaryEncoding += "1"
     except Exception as e:
-        print(e , "\n L1-Flag:0")
+        print(e)
         BinaryEncoding += "0"
     print()
 
     print("Sending the large ClientHello in two separate writes:")
-    sock = socket.create_connection(addr)
     try:
+        sock = socket.create_connection(addr)
         half = len(client_hello)//2
         sock.send(client_hello[:half])
         time.sleep(1)
         sock.send(client_hello[half:])
-        print(sock.recv(256) , "\n L2-Flag:1")
+        print(sock.recv(256))
         BinaryEncoding += "1"
     except Exception as e:
-        print(e , "\n L2-Flag:0")
+        print(e)
         BinaryEncoding += "0"
     print()
 
     client_hello = make_client_hello(host, kyber=False)
 
-    print(f"Repeating the process with a smaller ClientHello ({len(client_hello)} bytes).")
-    print("This ClientHello would usually be sent in a single packet, but it")
-    print("demonstrates that the bug is not triggered by the size of the")
-    print("ClientHello, but whether it comes in across multiple reads.")
-    print("(Note this ClientHello is smaller than a ClientHello from browsers")
-    print("today. This script does not reproduce some padding behavior.)")
-    print()
+    # print(f"Repeating the process with a smaller ClientHello ({len(client_hello)} bytes).")
+    # print("This ClientHello would usually be sent in a single packet, but it")
+    # print("demonstrates that the bug is not triggered by the size of the")
+    # print("ClientHello, but whether it comes in across multiple reads.")
+    # print("(Note this ClientHello is smaller than a ClientHello from browsers")
+    # print("today. This script does not reproduce some padding behavior.)")
+    # print()
 
     print("Sending the smaller ClientHello in a single write:")
-    sock = socket.create_connection(addr)
-    sock.send(client_hello)
+
     try:
-        print(sock.recv(256) , "\n S1-Flag:1")
+        sock = socket.create_connection(addr)
+        sock.send(client_hello)
+        print(sock.recv(256))
         BinaryEncoding += "1"
     except Exception as e:
-        print(e , "\n S1-Flag:0")
+        print(e)
         BinaryEncoding += "0"
     print()
 
     print("Sending the smaller ClientHello in two separate writes:")
-    sock = socket.create_connection(addr)
+    
     try:
+        sock = socket.create_connection(addr)
         half = len(client_hello)//2
         sock.send(client_hello[:half])
         time.sleep(1)
         sock.send(client_hello[half:])
-        print(sock.recv(256) , "\n S2-Flag:1")
+        print(sock.recv(256) , "\nS2-Flag:1")
         BinaryEncoding += "1"
     except Exception as e:
-        print(e , "\n S2-Flag:0")
+        print(e , "\nS2-Flag:0")
         BinaryEncoding += "0"
     
-    return BinaryEncoding
+    return (BinaryEncoding,host)
+
+
+print(tldr_dectector("8.8.8.8"))
