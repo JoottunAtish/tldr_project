3dc2d17033662137e790e16e1dcc4ce0fb39253b
Poshan Peeroo <poshanpeeroo@gmail.com>
2024-06-14 19:40:18 +0400

    tls_version_check

--- configurations/tls_filterer.py
+++ configurations/tls_filterer.py
@@ -1,5 +1,13 @@
-# use openssl to check if a connection is possible 
-# get the cipher suite list from the connection
+import socket
+import ssl
 
 
 
+def tls_version_check(ip_address, port):
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+    with socket.create_connection((ip_address, port)) as sock:
+        sock.settimeout(30)
+        with context.wrap_socket(sock, server_hostname=ip_address) as ssock:
+            return ssock.version()
